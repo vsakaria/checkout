c1f56075c863f2c260785a131028e1e29c1c4a1b
Vishal Sakaria <vsakaria@gmail.com>
2020-05-07 13:11:34 +0100

    ADD tests

--- src/components/Main/test/Main.test.tsx
+++ src/components/Main/test/Main.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { cleanup, render } from "@testing-library/react";
+import { cleanup, render, fireEvent, act } from "@testing-library/react";
 
 import Main from "../Main";
 
@@ -9,4 +9,19 @@ afterAll(cleanup);
 test("Main displays the correct title", () => {
   const { getByText } = render(<Main />);
   expect(getByText("Checkout.com")).toBeInTheDocument();
+});
+
+describe("Main", () => {
+  test("Main displays the correct title", async () => {
+    render(<Main />);
+    //Get star rate 3
+    const rateStar3 = document.querySelectorAll('[for="rating-3"]')[0];
+
+    //Simulate click on star
+    await act(async (): Promise<any> => fireEvent.click(rateStar3));
+    const selectClass = rateStar3.firstElementChild?.className;
+
+    //Expect star to be highlighted.
+    expect(selectClass).toContain("MuiRating-iconFilled");
+  });
 });
